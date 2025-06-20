999fe552397009a33a4bc046448029efefc18c85
benbell95 <benbell01@gmail.com>
2025-06-20 09:06:36 +0100

    fix to scaling + label rotation

--- example_workflow.r
+++ example_workflow.r
@@ -2,7 +2,64 @@
 
 # set wd
 setwd("~/code/r/graphics/better_biplot")
-# Load heather models
+
+
 # Local copy
 source("~/code/r/graphics/better_biplot/r/better_biplot.r")
+
+## Test with iris
+p <- prcomp(iris[-5])
+
+gr <- as.factor(iris[,5])
+
+layout(matrix(1:2, ncol=2, byrow=TRUE))
+bb_biplot(p, group=gr, scale=0, main="bens biplot scale =0", lab_rotation=TRUE, labd=1:200)
+bb_biplot(p, group=gr, scale=1, main="bens biplot scale =1", lab_rotation=TRUE, labd=1:200)
+
+
+
+p1 <- prcomp(USArrests)
+
+layout(matrix(1:2, ncol=2, byrow=TRUE))
+bb_biplot(p1, cols="blue", scale=0, main="bens biplot scale =0", lab_rotation=TRUE, labd=rownames(USArrests))
+bb_biplot(p1, cols="blue", scale=1, main="bens biplot scale =1", lab_rotation=TRUE, labd=rownames(USArrests))
+
+
+layout(matrix(1:2, ncol=2, byrow=TRUE))
+bb_biplot(p1, cols="blue", scale=1, main="bens biplot scale =0", lab_rotation=TRUE, labd=rownames(USArrests), ran_adj=TRUE)
+bb_biplot(p1, cols="blue", scale=1, main="bens biplot scale =1", lab_rotation=TRUE, labd=rownames(USArrests), ran_adj=FALSE)
+
+
+
+
+bb_biplot(p, group=gr, scale=0)
+
+
+layout(matrix(1:4, ncol=2, byrow=TRUE))
+bb_biplot(p, group=gr, scale=0, main="bens biplot scale =0")
+bb_biplot(p, group=gr, scale=1, main="bens biplot scale =1")
+
+# base r biplot
+
+#layout(matrix(1:2, ncol=2))
+biplot(p ,scale=0, main="base R biplot scale =0")
+biplot(p, scale=1, main="base R biplot scale =1")
+
+
+
+
+
+## Use vegan for comparison
+# biplot.rda function [use plot()]
+library(vegan)
+pv <- rda(iris[-5])
+plot(pv)
+
+
+# Compare vegan plot to biplot
+layout(matrix(1:8, ncol=4, byrow=TRUE))
+plot(pv, main="vegan biplot.rda scale=0", scaling =0)
+plot(pv, main="vegan biplot.rda scale=1", scaling =1)
+plot(pv, main="vegan biplot.rda scale=2", scaling =2)
+plot(pv, main="vegan biplot.rda scale=3", scaling =3)
 
--- r/better_biplot.r
+++ r/better_biplot.r
@@ -1,66 +1,114 @@
 ##########################################################################
-### Ben's better biplot
+### Ben's Better Biplot
 ### Plot function for creating better looking biplots in R (using base R)
 ### Copyright 2025, Benjamin Bell.
 ### Code and updates: https://github.com/benbell95/better_biplot
 ### Blog: https://www.benjaminbell.co.uk
+### Contact details via blog or github. Please send comments, suggestions, bug reports.
 
 ### This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-### This function has been tested with R v4.4.3. Use at your own risk.
+### This function has been tested with R v4.5.0. Use at your own risk.
 
-### See README.md file for usage.
+### Plots a biplot of pca data created with base R prcomp() function. Does not require any other packages or ggplot2. 
+### Should play nicely with layout(), and other plot functions, e.g. adding additional elements after plotting.
 
+### See README.md file for usage. Arguments listed below. Full guide also available on my blog (coming soon).
 
-# To do: Add ability to add legend
+# To do: 
+
+# Look at the other princomp (base R) scaling option - compare to vegan
+# Check Wills email, add the addtional functions he requested
+
+# Add ability to add legend
 # More customisation of plot elements
+# Add no fill option for ellipse and chull
+# Add checks for prcomp object
+# Check compatability with princomp
+# Change cols argument to col - and then fix function
 
+
+
+# *** Look at the pca papers, need to add rotation to the data?
 
 # Run PCA in R using prcomp()
 # Save output to an object, and use bb_biplot to produce a better looking biplot compared to the default base R biplot() function, e.g. bb_biplot(mypca)
 # All code in function is based on base R - does not require any other packages or ggplot2.
 # Data is scaled based on base R biplot() function. e.g. https://github.com/wch/r-source/blob/trunk/src/library/stats/R/biplot.R
 
-# See Readme file for details of usage, or visit my blog for examples.
+
+### Arguments / details
+
+# x             = The pca (prcomp) object. You must run prcomp() on your data before using this function.
+# pc1           = The first principal component to plot (default 1).
+# pc2           = The second principal component to plot (default 2).
+
+# scale         = Scale data between 0 and 1 for plotting. This works the same way as R base biplot() function.
+# limx          = x and ylim are generated automatically, but you can increase or decrease using a multiplier value (e.g. 1.5)
+# grid          = (logical). Draw a grid through center x and y axes.
+# cols          = Colours for the plot / groups. Optional. Uses default palette if not supplied. 
+# pch           = Plotting symbols to use for the observations (also see group 2)
+# cex.pt        = Changes the size of the plotting symbols.
+
+# group         = Groups for the data. Optional. Should be a factor, with length that matches original data. This affects the colour of the points  and also allows for ellipses or convex hulls to be drawn. This will also affect the plot symbol used if multiple pch supplied, unless group2 is also specified.
+# group2        = Second grouping of data. Optional. This affects the pch symbol only (e.g. multiple symbols within a single grouping). 
+
+# labd          = A vector of labels for the data observations, this should be the same length as the data. Use NULL values to omit some labels. Optional. 
+# colld         = Colours for the labels. Default is black.
+# cex.labd      = Size of text labels.
+
+# vectors       = (logical). Whether to plot the vectors or not.
+# whichv        = By default, all vectors are plotted, but you can instead specify which to plot. This can be a character vector, where names should exactly match the names of the vectors you want to plot, or a numeric vector to match index position.
+# sv            = Scale the vectors, if the vectors are too large or too small, change this value. Multiplier - values above 1 increase, while below 1 decrease the size.
+# colv          = Colour of the vector arrows.
+# colvt         = Colour of the vector arrow labels.
+# cex.labv      = Size of the text labels for the vectors.
+
+# lab_rotation  = (logical). Rotate labels for the data observations and vectors. Useful when you have lots of data points and labels. This may be slow if there are lots of labels to plot.
+# ran_adj     = (logical). Alter (adjustment) position of the labels for the data observations.
+# valign        = Alignment of labels for the vectors (accepted values = 0, 0.5 (default), 1)
+
+# chull         = (logical). Add convex hulls to the plot - group must be supplied for this to work.
+# ellipse       = (logical). Draw ellipses around the data observations based on the group. 
+# refine_ellipse = (logical). Checks the slope angle of the grouped data and flips ellipse if meets certain criteria. Change to TRUE if some of the ellipses seem like they should be the other way around.
+# se            = Scale the size of the ellipses. Multiplier - values above 1 increase, while below 1 decrease the size.
+
+# ...           = Additional arguments passed to plot() [main plot only].
 
 
-### Ben's better biplot
-### Arguments (options)
-# x = the pca (prcomp) object
-# pc1 = the first principal component to plot (default 1, but can change)
-# pc2 = the second principal component to plot (default 2, but can change)
-# scale = scale data between 0 and 1 (as per base biplot() function)
-# limx = x and ylim are generated automatically, you can increase/decrease using a multiplier value (e.g. 1.5)
-# grid (logical) = draw a grid through center x and y axes.
-# cols = Optional. Colours for the groups. (can be omitted to use default colour palette)
-# pch = plotting symbols to use (also see group 2)
-# group = Optional, but recommended! Groups for the data, should be a factor. (e.g. could be the sample, or species), length must match length of original data. This affects the colour of the points and allows for ellipses or convex hulls to be drawn. Will also affect the symbol used (if multiple pch supplied), unless group2 is also specified.
-# group2 = Optional. Second grouping of data - this only applies to the pch symbol used to plot the data.
-# labd = Optional. A vector of labels for the data observations.
-# lab_rotation (logical). = Rotate labels for the data observations. This can be slow if lots of labels.
-# vectors (logical) = Whether to draw vectors or not
-# whichv = Optional. Choose which vectors to draw (default is all of them). This can be a character vector, which should exactly match the names of the vectors you want to draw, or a numeric vector to match index position.
-# sv = scale vectors - change value if they appear off the biplot.
-# colv = Colour of the vector arrows
-# colvt = colour of the vector arrow labels
-# chull (logical) = Add convex hulls to the groups.
-# ellipse (logical) = Draw ellipses around the data observations based on group.
-# refine_ellipse (logical) = Checks slope angle of data and flips ellipse if meets certain criteria. Change to FALSE if weird results, or takes too long to plot.
-# cex.pt = changes size of points.
-# ... = additional arguments (applied to plot())
+### Ben's Better biplot function
+bb_biplot <- function(x, pc1=1, pc2=2, scale=1, pc.biplot=FALSE, limx, grid=TRUE, cols, pch=21, cex.pt=1, group, group2, labd, colld="black", cex.labd=1, vectors=TRUE, whichv, sv=1, colv="red", colvt="black", cex.labv=1, lab_rotation=FALSE, ran_adj=FALSE, valign=0.5, chull=FALSE, ellipse=FALSE, refine_ellipse=FALSE, se=1, ...) {
+    # Add check that x is a prcomp object [or princomp]
+    # not tested with princomp()
+    xclass <- class(x)
 
-bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, group, group2, labd, colld="black", cex.labd=1, lab_rotation=FALSE, vectors=TRUE, whichv, sv=1, colv="red", colvt="black", chull=FALSE, ellipse=FALSE, refine_ellipse=TRUE, se=1, cex.pt=1, cex.labv=1, ...) {
+    #if(scale < 0 || scale > 1) warning("scale should be a value between 0 and 1]")
+    
     ########################################
-    ### Scale data as per R base biplot() function
-    n <- nrow(x$x)
-    lam <- (x$sdev[c(pc1, pc2)] * sqrt(n)) ^ scale
-    rot <- t(t(x$rotation[, c(pc1, pc2)]) * lam)
-    sco <- t(t(x$x[, c(pc1, pc2)]) / lam)
+    ### Scale data
+    # Code modified from R base biplot() function code [Copyright (C) 1995-2012 The R Core Team]
+    # prcomp() scaling
+    if(xclass=="prcomp") {
+        n <- nrow(x$x)
+        if(scale != 0) {
+            lam <- (x$sdev[c(pc1, pc2)] * sqrt(n)) ^ scale 
+        } else {
+            lam <- 1
+        }
+        #if(pc.biplot==TRUE) lam <- lam / sqrt(n)      
+        sco <- t(t(x$x[, c(pc1, pc2)]) / lam)
+        rot <- t(t(x$rotation[, c(pc1, pc2)]) * lam)
+    }
+
     ########################################
     ### Plot data / config
     # Get x/ylim values
+    # Scores
     al <- ceiling(max(abs(range(sco)))*10) /10
     lim <- c(al*-1, al)
+    # Rotation
+    alr <- ceiling(max(abs(range(rot)))*10) /10
+    limr <- c(alr*-1, alr)
     # If plotting ellipses, and scale > 0, the limits will be too small, so always increase (unless limx is specified)
     if(ellipse==TRUE & scale > 0 & hasArg(limx) == FALSE) {
         limx <- 1.5
@@ -68,6 +116,7 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
     # Increase x/y lim values?
     if(hasArg(limx) == TRUE) {
         lim <- lim * limx
+        limr <- limr * limx
     }
     # Convex hulls
     if(chull==TRUE) {
@@ -89,7 +138,8 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
     }
     # Label rotation
     if(lab_rotation==TRUE) {
-        lsrt <- .bb_lab_rotation(x=sco)
+        lsrt <- .bb_lab_rotation(x=sco, valign=valign, ran_adj=ran_adj)
+        lvsrt <- .bb_lab_rotation(x=rot, valign=valign, ran_adj=FALSE)
     }
     ### Colour/style points by group (factor)
     # Overly complex due to outline and fill colours
@@ -155,9 +205,9 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
     ########################################
     ### Main Plot
     # Initial blank plot (plot order matters!)
+    op <- par(pty="s")
+    on.exit(par(op))
     plot(sco[,1], sco[,2], type="n", asp=1, xlim=lim, ylim=lim, xlab=xla, ylab=yla, ...)
-    axis(3)
-    axis(4)
     # Plot convex hulls
     if(chull==TRUE) {
         for(i in 1:nlevels(group)) {
@@ -186,7 +236,7 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
         # Check length matches data
         if(length(labd) != length(sco[,1])) message("Info: Number of labels does not match number of data observations")
         if(lab_rotation==TRUE) {
-            # rotation is not vectorised, so must run in a loop (Can be slow if lots of labels)
+            # srt rotation is not vectorised, so must run in a loop (Can be slow if lots of labels)
             for(i in 1:length(sco[,1])) {
                 text(sco[,1][i], sco[,2][i], labels=labd[i], col=colld, srt=lsrt[,1][i], adj=c(lsrt[,2][i], lsrt[,2][i]), cex=cex.labd, xpd=TRUE) 
             }
@@ -218,21 +268,37 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
                 rownames(rot) <- vname[match]
             }
         } 
+        # New plot window
+        par(new=TRUE)
+        plot(rot[,1], rot[,2], type="n", asp=1, xlim=limr, ylim=limr, ann=FALSE, axes=FALSE)
+        axis(3)
+        axis(4)
         # Get vector co-ordinates
         vx <- rot[,1]
         vy <- rot[,2]
         # Add
         arrows(x0=0, x1=vx*sv, y0=0, y1=vy*sv, col=colv, length=0.15, lwd=2, xpd=TRUE)
         # Labels
-        adjv<- sample.int(4L, length(vx), replace=TRUE) 
-        text(vx*sv, vy*sv, labels=rownames(rot), col=colvt, pos=adjv, cex=cex.labv, xpd=TRUE)
+        if(lab_rotation==TRUE) {
+            # srt rotation is not vectorised, so must run in a loop (Can be slow if lots of labels)
+            for(i in 1:length(rot[,1])) {
+                text(rot[,1][i], rot[,2][i], labels=rownames(rot)[i], col=colvt, srt=lvsrt[,1][i], adj=c(lvsrt[,2][i], lvsrt[,3][i]), cex=cex.labv, xpd=TRUE) 
+            }
+        } else {
+            # Random positions
+            adjv<- sample.int(4L, length(vx), replace=TRUE) 
+            text(vx*sv, vy*sv, labels=rownames(rot), col=colvt, pos=adjv, cex=cex.labv, xpd=TRUE)
+        }
     }
+    ### Legend
+    # Everybody needs a legend
 }
 
 
 ### Label rotation function
 # x = matrix of xy values for data (scores)
-.bb_lab_rotation <- function(x, alter_dir=TRUE) {
+### Remove this!
+.bb_lab_rotation_OLD <- function(x, alter_dir) {
     # Get orders of original data (by y)
     x_or <- order(x[,2])
     # Create rotation sequence for all data
@@ -254,6 +320,36 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
     # Return data
     return(cbind(x_r1, ad))
 }  
+
+### Vector label rotation function
+# x = matrix of xy values for vector arrows (x$rotation)
+.bb_lab_rotation <- function(x, valign, ran_adj) {
+    # Angle in degrees
+    x_an <- atan(x[,2] / x[,1]) * 180/pi
+    # text adj value
+    ad1 <- rep(0, times=nrow(x))
+    ad2 <- rep(valign, times=nrow(x))
+    # Adjust based on quadrant
+    bl <- which(x[,1] < 0 & x[,2] < 0)
+    tl <- which(x[,1] < 0 & x[,2] > 0)
+    # bottom left
+    if(length(bl)>0) {
+       ad1[bl] <- 1
+    }
+    # top left
+    if(length(tl)>0) {
+        ad1[tl] <- 1
+    }
+    # Random adjustment
+    if(ran_adj==TRUE) {
+        ad1 <- sample(c(0, 0.5, 1), length(ad1), replace=TRUE) 
+        ad2 <- sample(c(0, 0.5, 1), length(ad2), replace=TRUE) 
+    }
+    # Combine and return data
+    df <- cbind(x_an, ad1, ad2)
+    return(df)
+}
+
 
 ### Ellipse function
 # x should be 2 column df with x/y
@@ -284,6 +380,7 @@ bb_biplot <- function(x, pc1=1, pc2=2, scale=1, limx, grid=TRUE, cols, pch=21, g
     # Refine
     if(refine_ellipse==TRUE) {
         # Check slope of data, and flip ellipse if negative between -0 and -1
+        # ******** Check this with more data sets - needs refinement ********
         cslm <- lm(x[,1] ~ x[,2], data=x)
         if(coef(cslm)[2] < 0 & coef(cslm)[2]> -1) {
             x1 <- xc + (a*cos(t)*cos(phi) * -1) - b*sin(t)*sin(phi)

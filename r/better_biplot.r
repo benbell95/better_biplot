26fbf3dc5f94973490eb18f62914d2f38528e855
benbell95 <benbell01@gmail.com>
2025-06-26 09:10:55 +0100

    minor updates to help

--- r/better_biplot.r
+++ r/better_biplot.r
@@ -16,7 +16,7 @@
 ### Should play nicely with layout(), and other plot functions, e.g. adding additional elements after plotting, changing par() etc.
 
 ### See README.md file for usage. Arguments listed below. Full guide also available on my blog (coming soon).
-### Can also use .bb_info() to bring up help in the R console.
+### Can also use bb_info() to bring up help in the R console.
 
 ########################################
 ### Arguments / details
@@ -48,7 +48,7 @@
 # cex.labd      = Size of text labels.
 # font.labd     = Font type for data labels [1 = normal, 2 = bold, 3 = italic, 4 = bold italic].
 
-# variables     = Logical. Whether to plot the variables or not.
+# variables     = Logical. Whether to plot the variables or not [default = TRUE].
 # whichv        = Specify which variables to plot. Either character vector, where names should exactly match the variables, or integer values to match index position. If specified as "circle.eq", it will only plot variables extending beyond the circle of equilibrium contribution, overriding any other variables - must also specify circle.eq=TRUE and scale=0 to work.
 # expand        = Scale the variable arrows, if the arrows are too large or too small, change this value. Multiplier: values above 1 increase, while below 1 decrease the size.
 # arrow.len     = Length of arrow head. Use 0 to suppress.
